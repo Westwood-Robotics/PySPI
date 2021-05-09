b35859bcd13c18aa2307337186b7fde6305f50d4
Hawkry Zhang <hawkrybruin@gmail.com>
2021-05-08 23:37:56 -0700

    BCS_V1 test

---
+++ BCS_V1-TEST.py
@@ -0,0 +1,17 @@
+import spidev
+import wiringpi
+import time
+
+max_speed_hz = 2000
+spi_mode = 0
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = max_speed_hz
+spi.mode = spi_mode
+print("Device connected.")
+
+send = 0x1E
+spi.writebytes([send])
+data = spi.readbytes(2)
+
